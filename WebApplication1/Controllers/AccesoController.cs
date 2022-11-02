cf5069b66eca30c5873b8a1b3c925ce0db2869e5
josisan <softdevlearn@gmail.com>
2022-11-01 23:35:36 -0400

    Trabajando con el log-in y log-up

---
+++ WebApplication1/Controllers/AccesoController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+
+using WebApplication1.Models;
+
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        static readonly string cadena = "Data Sourse=DESKTOP-QRAVBP7; Initial Catalog=petHome; Integrated Security=true";
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Usuario oUsuario, SqlConnection cn, SqlConnection pn)
+        {
+            bool registrado;
+            string mensaje;
+
+            if (oUsuario.Clave == oUsuario.ConfirmarClave)
+            {
+                oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Las constraseñas no coinciden";
+                return View();
+            }
+
+            SqlConnection sqlConnection = new SqlConnection(cadena);
+            using (SqlConnection cn = sqlConnection)
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", cn);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                pn.Open();
+
+                cmd.ExecuteNonQuery();
+
+                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+            }
+
+            ViewData["Mensaje"] = mensaje;
+            if (registrado)
+            {
+                return RedirectToAction("Login", "Acceso");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+
+
+        public static string ConvertirSha256(string texto)
+        {
+            //using System.Text;
+            //Usar la referencia de "System.Security.Cryptography"
+
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256.Create())
+                {
+                    Encoding enc = Encoding.UTF8;
+                    byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+                    foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+
+                 }
+
+              return Sb.ToString();
+
+          }
+
+    }
+}
+
---
+++ WebApplication1/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace WebApplication1.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Clave { get; set; }
+
+        public string ConfirmarClave { get; set; }
+    }
+}
